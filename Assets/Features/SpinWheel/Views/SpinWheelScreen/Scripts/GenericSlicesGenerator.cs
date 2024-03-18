2502fcca13794653174f2c2c85fc3d7c16f28381
Saad Asif <saad.asif@hazelmobile.co>
2024-03-18 15:36:45 +0500

    Now slices text will always be in center!

--- Assets/Features/SpinWheel/Views/SpinWheelScreen/Scripts/GenericSlicesGenerator.cs
+++ Assets/Features/SpinWheel/Views/SpinWheelScreen/Scripts/GenericSlicesGenerator.cs
@@ -5,14 +5,18 @@ using UnityEngine;
 // Note : No.of slices inside spin wheel depends on your json file
 public class GenericSlicesGenerator : MonoBehaviour
 {
-    int minLimit = 2;
-    int maxLimit = 12;
-    float offset = -200;
+    int _minLimit = 2;
+    int _maxLimit = 12;
+
     private int totalSlicesInsideWheel;
+    
+    [SerializeField] private string _sliceTextPrefabName;
+    [SerializeField] private float _yTextsOffset = 260f;
 
     [SerializeField] string spinWheelSlicePrefabName;
  
     [SerializeField] Transform SlicesParent;
+    [SerializeField] Transform AllTextsParent;
     [SerializeField] JsonReaderSO JsonReaderSO;
     [SerializeField] IntVariable TotalSlicesSo;
 
@@ -30,23 +34,50 @@ public class GenericSlicesGenerator : MonoBehaviour
     {
         SpinnerFlowController.ResetSpinner += RedrawSpinner;
     }
+    
+    
+    [Button("GenerateTexts")]
+    private void GenerateTexts()
+    {
+        float currentAngle = 0;
+        float rotationangle = 360 / (float)totalSlicesInsideWheel;
+        DeletePrevious();
+        for (int i = 0; i < totalSlicesInsideWheel; i++)
+        {
+            GameObject textPrefb = Instantiate(Resources.Load(_sliceTextPrefabName),AllTextsParent)as GameObject;
+            textPrefb.transform.Rotate(0,0,currentAngle);
+            Vector2 pos = textPrefb.transform.GetChild(0).transform.localPosition;
+            pos.y += _yTextsOffset;
+            textPrefb.transform.GetChild(0).transform.localPosition = pos;
+            currentAngle += rotationangle;
+        }
+    }
+    private void DeletePrevious()
+    {
+        print(AllTextsParent.childCount);
+        for (int i = AllTextsParent.childCount-1; i >= 0; i--)
+        {
+            DestroyImmediate(AllTextsParent.GetChild(i).gameObject); 
+        }
+    }
 
     [Button("Generate Spin Wheel Slices")]
     private void GenerateSpinWheelSlices()
     {
         JsonReaderSO.LoadDataFromFile();
         totalSlicesInsideWheel = TotalSlicesSo.value;
-        if (totalSlicesInsideWheel >= minLimit && totalSlicesInsideWheel <= maxLimit)
+        if (totalSlicesInsideWheel >= _minLimit && totalSlicesInsideWheel <= _maxLimit)
         {
             RedrawSpinner();
         }
         else
-            Debug.LogError($"Out of Range!! No of slices must be between {minLimit} to {maxLimit}");
+            Debug.LogError($"Out of Range!! No of slices must be between {_minLimit} to {_maxLimit}");
     }
     private void RedrawSpinner()
     {
         Regenerate();
         SpawnSlices();
+        GenerateTexts();
     }
   
     private void Regenerate()
